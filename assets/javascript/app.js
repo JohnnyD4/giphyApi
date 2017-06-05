efb94454cf36e6358dbf6354ad352de90c36799f
John Davis <johnsdavis95@gmail.com>
2017-06-05 09:59:29 -0400

    Almost finished hw

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	var limit = 2;
 
 	var topics = ["Miami Hurricanes", "Miami Dolphins", "Chelsea FC", "Real Madrid"];
-
+var animate = 0;
 	renderButtons();
 	displayGiphy();
 
@@ -18,6 +18,8 @@ $(document).ready(function() {
 
 		$(".teams").on("click", function() {
 			
+			
+
 			offset = limit;	
 			
 			var giph = $(this).attr("data-name");
@@ -35,46 +37,65 @@ $(document).ready(function() {
 	        	var results = response.data;
 
 	        	for (var i = 0; i < results.length; i++) {
-	        		
+	        		var imgDiv = $("<div class='imDiv'>");
+
 	        		var rating = results[i].rating;
 
 	        		var p = $("<p>").text("Rating: " + rating);
 
-	        		var giphImage = $("<img>");
+	        		var giphImage = $("<img class='giphStill'>");
 
-	        		var giphMove = $("<img>");
+	        		//var giphMove = $("<img>");
 
-	        		giphImage.attr({
-	        			"src": results[i].images.fixed_height_still.url
-	        		})
+	        		giphImage.attr("src", results[i].images.fixed_width_still.url).val(i)
 
-	        		giphMove.attr({
-							"src": results[i].images.fixed_height.url
+	        		//giphMove.attr("src", results[i].images.fixed_width.url).val(i)
+
+	        		$(imgDiv).prepend(p);
+
+	        		$(imgDiv).prepend(giphImage);
+
+	        		$("#giphsHere").prepend(imgDiv);
+
+					$(".giphStill").on("click", function(event) {
+						console.log("hello");
+						console.log(this.value)
+						if (animate == 0){ 
+							
+                   			$(this).attr("src", response.data[this.value].images.fixed_width.url);
+                    		animate++
+                		}else{
+                    		$(this).attr("src", response.data[this.value].images.fixed_width_still.url);
+                    		animate--
+                		}
+
+
+
+
+
+
+
+					// 	if (giphImage.val == giphImage.val) {
+
+					// 	console.log(giphImage.val());
+						
+						
+					// 	displayGiphy();
+					// 	} else {
+					// 		console.log(i);
+					// 		console.log(giphImage.val("id"));
+					// 	console.log(" doesnt work");
+					// }
 					})
 
-	        		$("#giphsHere").prepend(p);
+					// $(giphMove).on("click", function() {
+						
+					// 	giphMove.hide();
+						
+					// 	giphImage.show();
 
-	        		$("#giphsHere").prepend(giphImage);
-
-					$(giphImage).on("click", function() {
-						
-						giphImage.hide();
-						
-						$("#giphsHere").prepend(giphMove);
-						
-						giphMove.show();
-						
-						displayGiphy();
-					})
-
-					$(giphMove).on("click", function() {
-						
-						giphMove.hide();
-						
-						giphImage.show();
-
-						displayGiphy();
-					})
+					// 	displayGiphy();
+					// })
 	        		
 	        	} 
 	        
